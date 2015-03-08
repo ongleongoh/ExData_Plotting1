a69c69faa4115bb9300b9f69b366b9d2233edb51
ong leong oh <onglo@yahoo.com.sg>
2015-03-08 21:23:26 +0800

    Re-submit

--- Plot4.r
+++ Plot4.r
@@ -19,16 +19,15 @@ par(mfcol=c(2,2))
 plot(df$DateTime, df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
 
 ## Create plot 2
-png(filename="plot3.png", width=480, height=480)
-plot(df$DateTime, df$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
+plot(df$DateTime, df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(df$DateTime, df$Sub_metering_2, col="red")
 lines(df$DateTime, df$Sub_metering_3, col="blue")
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       col=c("black", "red", "blue"), lwd=par("lwd"))
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lwd=par("lwd" ), bty="n")
 
 ## Create plot 3
 plot(df$DateTime, df$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
 ## Create plot 4
 plot(df$DateTime, df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
-dev.off() 
+
+dev.off()
